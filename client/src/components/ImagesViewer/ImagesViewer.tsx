c3fd6a23893f613cc6aa8aa65314bd44fdb769c9
mo7amedsakr <msakr456@gmail.com>
2020-07-28 11:39:21 +0200

    lightbox

--- client/src/components/ImagesViewer/ImagesViewer.tsx
+++ client/src/components/ImagesViewer/ImagesViewer.tsx
@@ -1,21 +1,32 @@
 import React, { FC, useState } from 'react';
 import { Container, CurrentImg, Imgs } from './ImagesViewerStyle';
+import { Lightbox } from '../Lightbox/Lightbox';
 
 export interface ImagesViewerProps {
   images: string[];
 }
 
 export const ImagesViewer: FC<ImagesViewerProps> = (props) => {
-  const [currentImg, setCurrentImg] = useState(props.images[0]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleLightbox = () => setIsOpen((prev) => !prev);
 
   return (
     <Container>
+      {isOpen ? (
+        <Lightbox
+          images={props.images}
+          startIndex={currentIndex}
+          close={toggleLightbox}
+        />
+      ) : null}
       <CurrentImg>
-        <img src={currentImg} alt="" />
+        <img src={props.images[currentIndex]} alt="" onClick={toggleLightbox} />
       </CurrentImg>
       <Imgs>
         {props.images.map((img, i) => (
-          <img key={i} src={img} alt="" onClick={() => setCurrentImg(img)} />
+          <img key={i} src={img} alt="" onClick={() => setCurrentIndex(i)} />
         ))}
       </Imgs>
     </Container>

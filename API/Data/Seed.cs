82d5149a6ccab6e1061632fd1039d2fb8d969374
mo7amedsakr <msakr456@gmail.com>
2020-11-26 18:42:02 +0200

    seed admin user

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -1,15 +1,22 @@
 ﻿using API.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace API.Data
 {
 	public class Seed
 	{
+		public static async Task AddAdminUser(UserManager<User> userManager, RoleManager<Role> roleManager)
+		{
+			if ((await userManager.FindByEmailAsync("admin@admin.com")) == null)
+			{
+				var user = new User { Email = "admin@admin.com", UserName = "admin", Role = await roleManager.FindByNameAsync("Admin") };
+				await userManager.CreateAsync(user, "Pa$$w0rd");
+				await userManager.AddToRoleAsync(user, "Admin");
+			}
+		}
 		public static async Task SeedRoles(RoleManager<Role> roleManager)
 		{
 			if (await roleManager.Roles.AnyAsync()) return;
@@ -20,7 +27,6 @@ namespace API.Data
 			{
 				await roleManager.CreateAsync(role);
 			}
-
 		}
 	}
 }

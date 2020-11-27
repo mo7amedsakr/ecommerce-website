7dbd99bd60a204d5c5658e840d2def1dde94e536
mo7amedsakr <msakr456@gmail.com>
2020-11-27 04:57:31 +0200

    remove unused usings and sort

--- API/Data/Migrations/20201127011146_ChangedProductIdTypeToInt.cs
+++ API/Data/Migrations/20201127011146_ChangedProductIdTypeToInt.cs
@@ -1,32 +1,32 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
 
 namespace API.Data.Migrations
 {
-    public partial class ChangedProductIdTypeToInt : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<int>(
-                name: "Id",
-                table: "Products",
-                type: "INTEGER",
-                nullable: false,
-                oldClrType: typeof(Guid),
-                oldType: "TEXT")
-                .Annotation("Sqlite:Autoincrement", true);
-        }
+	public partial class ChangedProductIdTypeToInt : Migration
+	{
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.AlterColumn<int>(
+					name: "Id",
+					table: "Products",
+					type: "INTEGER",
+					nullable: false,
+					oldClrType: typeof(Guid),
+					oldType: "TEXT")
+					.Annotation("Sqlite:Autoincrement", true);
+		}
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<Guid>(
-                name: "Id",
-                table: "Products",
-                type: "TEXT",
-                nullable: false,
-                oldClrType: typeof(int),
-                oldType: "INTEGER")
-                .OldAnnotation("Sqlite:Autoincrement", true);
-        }
-    }
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.AlterColumn<Guid>(
+					name: "Id",
+					table: "Products",
+					type: "TEXT",
+					nullable: false,
+					oldClrType: typeof(int),
+					oldType: "INTEGER")
+					.OldAnnotation("Sqlite:Autoincrement", true);
+		}
+	}
 }
--- API/Dtos/UpdateProductDto.cs
+++ API/Dtos/UpdateProductDto.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace API.Dtos
+﻿namespace API.Dtos
 {
 	public record UpdateProductDto(decimal Price, string Description, int Quantity);
 }

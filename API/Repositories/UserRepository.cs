bb589b29fd2179e8ea15692828deec5986fa809c
mo7amedsakr <msakr456@gmail.com>
2020-11-27 17:58:58 +0200

    tabs

--- API/Repositories/UserRepository.cs
+++ API/Repositories/UserRepository.cs
@@ -12,28 +12,28 @@ using System.Threading.Tasks;
 
 namespace API.Repositories
 {
-  public class UserRepository : IUserRepository
-  {
-    private readonly DataContext _context;
-    private readonly IMapper _mapper;
+	public class UserRepository : IUserRepository
+	{
+		private readonly DataContext _context;
+		private readonly IMapper _mapper;
 
-    public UserRepository(DataContext context, IMapper mapper)
-    {
-      _context = context;
-      _mapper = mapper;
-    }
+		public UserRepository(DataContext context, IMapper mapper)
+		{
+			_context = context;
+			_mapper = mapper;
+		}
 
-    public async Task<UserDto> GetUserByIdAsync(Guid id)
-    {
-      return await _context.Users
-        .Where(u => u.Id == id)
-        .ProjectTo<UserDto>(_mapper.ConfigurationProvider)
-        .FirstOrDefaultAsync();
-    }
+		public async Task<UserDto> GetUserByIdAsync(Guid id)
+		{
+			return await _context.Users
+				.Where(u => u.Id == id)
+				.ProjectTo<UserDto>(_mapper.ConfigurationProvider)
+				.FirstOrDefaultAsync();
+		}
 
-    public async Task<IEnumerable<User>> GetUsersAsync()
-    {
-      return await _context.Users.Include(u => u.Role).ToListAsync();
-    }
-  }
+		public async Task<IEnumerable<User>> GetUsersAsync()
+		{
+			return await _context.Users.Include(u => u.Role).ToListAsync();
+		}
+	}
 }
